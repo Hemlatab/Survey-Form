bd2cdf8f0d1b218f38da0f2357217ce4c901d57e
Hemlatab <hemlata.b@hotmail.co.uk>
2020-04-06 18:51:24 +0400

    Update script

--- styles/validation.js
+++ styles/validation.js
@@ -1,12 +1,1 @@
-<script>
 
-function checkAge() {
-  let validateAge = document.getElementById("age");
-  if (!checkAge.checkAgeValidity()) {
-    document.getElementById("age").innerHTML = validateAge.validateMessage;
-  } else {
-    document.getElementById("age").innerHTML = "Age requirement met";
-  }
-}
-
-</script>
